de0608d8403c794802cd2ec300e26f6ad48f815e
AbdellahRAISSOUNI <87619389+AbdellahRAISSOUNI@users.noreply.github.com>
2024-04-03 18:01:07 +0000

    Add files via upload

--- client.c
+++ client.c
@@ -1,98 +1,144 @@
+#define _WIN32_WINNT 0x0501
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+
 #pragma comment(lib, "ws2_32.lib")
 
 #define BUFFER_SIZE 1024
+
+void handleServerMenu(SOCKET connectSocket, char* type) {
+    char menu[BUFFER_SIZE];
+    int choice;
+
+    recv(connectSocket, menu, sizeof(menu), 0);
+    printf("%s", menu);
+
+    while (1) {
+        if (scanf("%d", &choice) != 1) {
+            printf("Entrée invalide. Veuillez entrer un nombre entier.\n");
+            // Vidage du tampon d'entrée
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+            continue;
+        }
+
+        // Vidage du tampon d'entrée
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+
+        if (choice >= 1 && choice <= 6) {
+            break;
+        } else {
+            printf("Choix invalide. Veuillez entrer un nombre entre 1 et 6.\n");
+        }
+    }
+
+    send(connectSocket, (char*)&choice, sizeof(int), 0);
+
+    char message[BUFFER_SIZE];
+    recv(connectSocket, message, sizeof(message), 0);
+    printf("%s\n", message);
+}
+
 
 int main() {
     WSADATA wsaData;
     int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
-        printf("WSAStartup failed: %d\n", iResult);
+        printf("Erreur lors de l'initialisation de Winsock: %d\n", iResult);
         return 1;
     }
 
-    SOCKET client_socket;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
+    SOCKET connectSocket = INVALID_SOCKET;
+    struct addrinfo* result = NULL, * ptr = NULL, hints;
 
-    // Créer le socket client
-    client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (client_socket == INVALID_SOCKET) {
-        printf("Error creating socket: %d\n", WSAGetLastError());
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    iResult = getaddrinfo("127.0.0.1", "8000", &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
         WSACleanup();
         return 1;
     }
 
-    // Configurer l'adresse du serveur
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(8888);
-
-    // Se connecter au serveur
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        printf("Error connecting to server: %d\n", WSAGetLastError());
-        closesocket(client_socket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Demander le nom d'utilisateur et le mot de passe
-    printf("Entrez votre nom d'utilisateur : ");
-    fgets(buffer, BUFFER_SIZE, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0'; // Supprimer le caractère de nouvelle ligne
-    if (send(client_socket, buffer, strlen(buffer), 0) == SOCKET_ERROR) {
-        printf("Error sending data: %d\n", WSAGetLastError());
-        closesocket(client_socket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("Entrez votre mot de passe : ");
-    fgets(buffer, BUFFER_SIZE, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0';
-    if (send(client_socket, buffer, strlen(buffer), 0) == SOCKET_ERROR) {
-        printf("Error sending data: %d\n", WSAGetLastError());
-        closesocket(client_socket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Recevoir le menu du serveur
-    if (recv(client_socket, buffer, BUFFER_SIZE, 0) == SOCKET_ERROR) {
-        printf("Error receiving data: %d\n", WSAGetLastError());
-        closesocket(client_socket);
-        WSACleanup();
-        return 1;
-    }
-    printf("%s", buffer);
-
-    while (1) {
-        printf("Entrez votre choix : ");
-        fgets(buffer, BUFFER_SIZE, stdin);
-        buffer[strcspn(buffer, "\n")] = '\0';
-        if (send(client_socket, buffer, strlen(buffer), 0) == SOCKET_ERROR) {
-            printf("Error sending data: %d\n", WSAGetLastError());
-            closesocket(client_socket);
+    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+        connectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (connectSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %d\n", WSAGetLastError());
             WSACleanup();
             return 1;
         }
 
-        // Recevoir la réponse du serveur
-        if (recv(client_socket, buffer, BUFFER_SIZE, 0) == SOCKET_ERROR) {
-            printf("Error receiving data: %d\n", WSAGetLastError());
-            closesocket(client_socket);
-            WSACleanup();
-            return 1;
+        iResult = connect(connectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(connectSocket);
+            connectSocket = INVALID_SOCKET;
+            continue;
         }
-        printf("%s\n", buffer);
+        break;
     }
 
-    closesocket(client_socket);
+    freeaddrinfo(result);
+
+    if (connectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 1;
+    }
+
+    char username[50], password[50];
+    int attempts = 0;
+    char type[10]; // Declare type here
+
+    while (attempts < 3) {
+        printf("Nom d'utilisateur: ");
+        scanf("%s", username);
+        printf("Mot de passe: ");
+        scanf("%s", password);
+
+        send(connectSocket, username, strlen(username), 0);
+        send(connectSocket, password, strlen(password), 0);
+
+        recv(connectSocket, type, sizeof(type), 0);
+
+        if (strcmp(type, "admin") == 0) {
+            printf("*** Vous êtes admin ***\n");
+            break;
+        } else if (strcmp(type, "invite") == 0) {
+            printf("*** Vous êtes invité ***\n");
+            break;
+        } else {
+            printf("Échec de l'authentification\n");
+            attempts++;
+        }
+    }
+
+    if (attempts == 3) {
+        printf("Trop de tentatives échouées\n");
+        closesocket(connectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    int quit = 0;
+    int choice; // Declare choice here
+
+    while (!quit) {
+        handleServerMenu(connectSocket, type);
+
+        if ((strcmp(type, "admin") == 0 && choice == 6) || (strcmp(type, "invite") == 0 && choice == 3)) {
+            quit = 1;
+        }
+    }
+
+    closesocket(connectSocket);
     WSACleanup();
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,186 +1,248 @@
+#define _WIN32_WINNT 0x0501
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+
 #pragma comment(lib, "ws2_32.lib")
 
-#define MAX_CLIENTS 5
+#define MAX_CLIENTS 10
 #define BUFFER_SIZE 1024
 
-// Prototypes des fonctions
-void authentifier_utilisateur(SOCKET client_socket, char *username, char *password);
-void menu_admin(SOCKET client_socket);
-void menu_invite(SOCKET client_socket);
+typedef struct {
+    char username[50];
+    char password[50];
+    char type[10];
+} User;
+
+User users[100];
+int numUsers = 0;
+
+void loadUsers() {
+    FILE* file = fopen("comptes.txt", "r");
+    if (file == NULL) {
+        printf("Erreur lors de l'ouverture du fichier comptes.txt\n");
+        return;
+    }
+
+    char line[100];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        sscanf(line, "%s %s %s", users[numUsers].username, users[numUsers].password, users[numUsers].type);
+        numUsers++;
+    }
+
+    fclose(file);
+}
+
+int authenticateUser(char* username, char* password) {
+    for (int i = 0; i < numUsers; i++) {
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void sendMenu(SOCKET clientSocket, char* userType) {
+    if (strcmp(userType, "admin") == 0) {
+        char menu[] = "*********************Menu*********************\n"
+                      "1-Ajouter un contact\n"
+                      "2-Rechercher un contact\n"
+                      "3-Supprimer un contact\n"
+                      "4-Modifier un contact\n"
+                      "5-Afficher tous les contacts\n"
+                      "6-Quitter\n"
+                      "Entrer votre choix: ";
+        send(clientSocket, menu, strlen(menu), 0);
+    } else if (strcmp(userType, "invite") == 0) {
+        char menu[] = "*********************Menu*********************\n"
+                      "1-Rechercher un contact\n"
+                      "2-Afficher tous les contacts\n"
+                      "3-Quitter\n"
+                      "Entrer votre choix: ";
+        send(clientSocket, menu, strlen(menu), 0);
+    }
+}
+
+void handleClient(SOCKET clientSocket, char* userType) {
+    int choice;
+    int quit = 0;
+
+    while (!quit) {
+        sendMenu(clientSocket, userType); // Envoyer le menu approprié
+
+        recv(clientSocket, (char*)&choice, sizeof(int), 0);
+        handleClientChoice(clientSocket, userType, choice);
+
+        if ((strcmp(userType, "admin") == 0 && choice == 6) || (strcmp(userType, "invite") == 0 && choice == 3)) {
+            quit = 1;
+        }
+    }
+}
+
+void handleClientChoice(SOCKET clientSocket, char* userType, int choice) {
+    char message[BUFFER_SIZE];
+
+    if (strcmp(userType, "admin") == 0) {
+        switch (choice) {
+            case 1:
+                strcpy(message, "Vous avez choisi l'option 1 - Ajouter un contact");
+                break;
+            case 2:
+                strcpy(message, "Vous avez choisi l'option 2 - Rechercher un contact");
+                break;
+            case 3:
+                strcpy(message, "Vous avez choisi l'option 3 - Supprimer un contact");
+                break;
+            case 4:
+                strcpy(message, "Vous avez choisi l'option 4 - Modifier un contact");
+                break;
+            case 5:
+                strcpy(message, "Vous avez choisi l'option 5 - Afficher tous les contacts");
+                break;
+            case 6:
+                strcpy(message, "Vous avez choisi l'option 6 - Quitter");
+                break;
+            default:
+                strcpy(message, "Option invalide");
+                break;
+        }
+    } else if (strcmp(userType, "invite") == 0) {
+        switch (choice) {
+            case 1:
+                strcpy(message, "Vous avez choisi l'option 1 - Rechercher un contact");
+                break;
+            case 2:
+                strcpy(message, "Vous avez choisi l'option 2 - Afficher tous les contacts");
+                break;
+            case 3:
+                strcpy(message, "Vous avez choisi l'option 3 - Quitter");
+                break;
+            default:
+                strcpy(message, "Option invalide");
+                break;
+        }
+    }
+
+    int bytesSent = send(clientSocket, message, strlen(message), 0);
+    if (bytesSent != strlen(message)) {
+        printf("Erreur lors de l'envoi du message au client\n");
+    }
+}
 
 int main() {
     WSADATA wsaData;
     int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
-        printf("WSAStartup failed: %d\n", iResult);
+        printf("Erreur lors de l'initialisation de Winsock: %d\n", iResult);
         return 1;
     }
 
-    SOCKET server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    int client_len;
-    char buffer[BUFFER_SIZE];
-    SOCKET clients[MAX_CLIENTS];
-    int num_clients = 0;
+    loadUsers();
 
-    // Créer le socket serveur
-    server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (server_socket == INVALID_SOCKET) {
-        printf("Error creating socket: %d\n", WSAGetLastError());
+    SOCKET listeningSocket = INVALID_SOCKET;
+    struct addrinfo* result = NULL, hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    iResult = getaddrinfo(NULL, "8000", &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
         WSACleanup();
         return 1;
     }
 
-    // Lier le socket serveur à une adresse
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8888);
-
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        printf("Error binding socket: %d\n", WSAGetLastError());
-        closesocket(server_socket);
+    listeningSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (listeningSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %d\n", WSAGetLastError());
+        freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
 
-    // Écouter les connexions entrantes
-    if (listen(server_socket, MAX_CLIENTS) == SOCKET_ERROR) {
-        printf("Error listening on socket: %d\n", WSAGetLastError());
-        closesocket(server_socket);
+    iResult = bind(listeningSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(listeningSocket);
         WSACleanup();
         return 1;
     }
 
-    printf("Le serveur écoute sur le port 8888...\n");
+    freeaddrinfo(result);
+
+    if (listen(listeningSocket, SOMAXCONN) == SOCKET_ERROR) {
+        printf("Listen failed with error: %d\n", WSAGetLastError());
+        closesocket(listeningSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Serveur en attente de connexions...\n");
+
+    SOCKET clientSocket;
+    struct sockaddr_in clientAddr;
+    int clientAddrLen = sizeof(clientAddr);
 
     while (1) {
-        client_len = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
-        if (client_socket == INVALID_SOCKET) {
-            printf("Error accepting connection: %d\n", WSAGetLastError());
-            continue;
+        clientSocket = accept(listeningSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        if (clientSocket == INVALID_SOCKET) {
+            printf("accept failed: %d\n", WSAGetLastError());
+            closesocket(listeningSocket);
+            WSACleanup();
+            return 1;
         }
 
-        printf("Nouveau client connecté\n");
+        printf("Nouvelle connexion acceptée\n");
 
-        // Ajouter le client à la liste des clients connectés
-        clients[num_clients++] = client_socket;
+        char username[50], password[50];
+        int attempts = 0;
+        int userIndex;
 
-        // Authentifier l'utilisateur
-        char username[BUFFER_SIZE];
-        char password[BUFFER_SIZE];
-        authentifier_utilisateur(client_socket, username, password);
-
-        // Vérifier si l'utilisateur est admin ou invité
-        if (strcmp(username, "admin") == 0 && strcmp(password, "admin123") == 0) {
-            menu_admin(client_socket);
-        } else {
-            menu_invite(client_socket);
-        }
-
-        // Supprimer le client de la liste des clients connectés
-        closesocket(client_socket);
-        for (int i = 0; i < num_clients; i++) {
-            if (clients[i] == client_socket) {
-                clients[i] = clients[--num_clients];
-                break;
+        while (attempts < 3) {
+            int bytesReceived = recv(clientSocket, username, sizeof(username), 0);
+            if (bytesReceived <= 0) {
+                break; // Connection closed or error occurred
             }
+            username[bytesReceived] = '\0'; // Null-terminate the received string
+
+            bytesReceived = recv(clientSocket, password, sizeof(password), 0);
+            if (bytesReceived <= 0) {
+                break; // Connection closed or error occurred
+            }
+            password[bytesReceived] = '\0'; // Null-terminate the received string
+
+            userIndex = authenticateUser(username, password);
+            if (userIndex != -1) {
+                char type[10];
+                strcpy(type, users[userIndex].type);
+                send(clientSocket, type, sizeof(type), 0);
+                printf("Utilisateur %s (%s) connecté\n", username, type);
+
+                handleClient(clientSocket, type); // Handle client requests
+
+                break;
+            } else {
+                char message[] = "Échec de l'authentification";
+                send(clientSocket, message, sizeof(message), 0);
+                attempts++;
+            }
+        }
+
+        if (attempts == 3) {
+            printf("Trop de tentatives pour %s\n", username);
+            closesocket(clientSocket);
         }
     }
 
-    closesocket(server_socket);
+    closesocket(listeningSocket);
     WSACleanup();
+
     return 0;
 }
 
-void authentifier_utilisateur(SOCKET client_socket, char *username, char *password) {
-    char buffer[BUFFER_SIZE];
-
-    // Recevoir le nom d'utilisateur du client
-    recv(client_socket, buffer, BUFFER_SIZE, 0);
-    strcpy(username, buffer);
-
-    // Recevoir le mot de passe du client
-    recv(client_socket, buffer, BUFFER_SIZE, 0);
-    strcpy(password, buffer);
-
-    printf("Nom d'utilisateur reçu : %s, mot de passe : %s\n", username, password);
-}
-
-void menu_admin(SOCKET client_socket) {
-    char buffer[BUFFER_SIZE];
-
-    // Envoyer le menu admin au client
-    strcpy(buffer, "Menu administrateur :\n1. Ajouter un contact\n2. Modifier un contact\n3. Supprimer un contact\n4. Rechercher un contact\n5. Quitter\n");
-    send(client_socket, buffer, strlen(buffer), 0);
-
-    while (1) {
-        // Recevoir le choix du client
-        recv(client_socket, buffer, BUFFER_SIZE, 0);
-        int choix = atoi(buffer);
-
-        switch (choix) {
-            case 1:
-                // Ajouter un contact
-                printf("Le client a choisi d'ajouter un contact\n");
-                break;
-            case 2:
-                // Modifier un contact
-                printf("Le client a choisi de modifier un contact\n");
-                break;
-            case 3:
-                // Supprimer un contact
-                printf("Le client a choisi de supprimer un contact\n");
-                break;
-            case 4:
-                // Rechercher un contact
-                printf("Le client a choisi de rechercher un contact\n");
-                break;
-            case 5:
-                // Quitter
-                printf("Le client a choisi de quitter\n");
-                return;
-            default:
-                printf("Choix invalide\n");
-                break;
-        }
-    }
-}
-
-void menu_invite(SOCKET client_socket) {
-    char buffer[BUFFER_SIZE];
-
-    // Envoyer le menu invité au client
-    strcpy(buffer, "Menu invité :\n1. Afficher les contacts\n2. Rechercher un contact\n3. Quitter\n");
-    send(client_socket, buffer, strlen(buffer), 0);
-
-    while (1) {
-        // Recevoir le choix du client
-        recv(client_socket, buffer, BUFFER_SIZE, 0);
-        int choix = atoi(buffer);
-
-        switch (choix) {
-            case 1:
-                // Afficher les contacts
-                printf("Le client a choisi d'afficher les contacts\n");
-                break;
-            case 2:
-                // Rechercher un contact
-                printf("Le client a choisi de rechercher un contact\n");
-                break;
-            case 3:
-                // Quitter
-                printf("Le client a choisi de quitter\n");
-                return;
-            default:
-                printf("Choix invalide\n");
-                break;
-        }
-    }
-}
